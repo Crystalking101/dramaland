a99b28a4c5c0ffd724ec05ccb04964411af61fd0
Crystal King <crystalking@macbookpro.myfiosgateway.com>
2026-03-31 13:46:45 -0400

    add viewport meta tag for mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fontsource/playfair-display@5.0.8/index.css"/>
       </head>
       <body>{children}</body>
