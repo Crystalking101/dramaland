1ce901240087d45d7d4bfd8ea61822d44318ad3c
Crystal King <crystalking@macbookpro.myfiosgateway.com>
2026-04-17 14:37:16 -0400

    add Google Search Console verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="google-site-verification" content="HFA9fgH8YkH6NIi9x" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fontsource/playfair-display@5.0.8/index.css"/>
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-L4FTNE24FJ"></script>
         <script dangerouslySetInnerHTML={{__html: `
